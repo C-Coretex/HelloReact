cb2e91c108428eaf6a99161f9825415148e13eaf
valerash <valerash2@gmail.com>
2020-07-16 20:37:50 +0300

    added back

---
+++ Back-end/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NN;
+
+namespace Back_end
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = FileReaderMNIST.LoadImagesAndLables(
+                "./data/train-labels-idx1-ubyte.gz",
+                "./data/train-images-idx3-ubyte.gz");
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
