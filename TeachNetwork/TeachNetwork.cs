e9f0332d71bf7c2ece1f55851d71fef54b2d2960
C-Coretex <valerash2@gmail.com>
2020-07-19 11:19:41 +0300

    Added MNIST dataset using Accord.DataSets library

---
+++ TeachNetwork/TeachNetwork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NN;
+using Accord.DataSets;
+
+namespace Trach
+{
+	class TeachNetwork
+	{
+		static void Main()
+		{
+			Console.WriteLine("Hello World!");
+
+			MNIST data = new MNIST();
+
+			Console.Write("Completed");
+		}
+	}
+}
